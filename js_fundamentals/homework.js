57d84e990627384655b7fea2e0c80f9c1aa5d260
9338190 <a9338190@9338190s-MacBook-Pro.local>
2019-04-02 22:25:23 +0100

    (almost)completed homework

--- js_fundamentals/homework.js
+++ js_fundamentals/homework.js
@@ -8,84 +8,84 @@
 //
 // <!-- 1.1
 // ```js
-// 7 + 3;
+console.log(7 + 3);
 // ```
 //
 // 1.2
 // ```js
-// 9 % 2
+console.log(9 % 2);
 // ```
 //
 // 1.3
 // ```js
-// typeof(42);
+console.log(typeof(42));
 // ```
 //
 // 1.4
 // ```js
-// typeof(2) === typeof(9.24);
+console.log(typeof(2) === typeof(9.24));
 // ```
 //
 // 1.5
 // ```js
-// 42 / 0;
+console.log(42 / 0);
 // ```
 //
 // 1.6
 // ```js
-// 9 / "player1";
+console.log(9 / "player1");
 // ```
 //
 // 1.7
 // ```js
-// NaN === NaN;
+console.log(NaN === NaN);
 // ```
 //
 // 1.8
 // ```js
-// typeof(NaN);
+console.log(typeof(NaN));
 // ```
 //
 // 1.9
 // ```js
-// typeof parseInt("42");
+console.log(typeof parseInt("42"));
 // ```
 //
 // 1.10
 // ```js
-// "Brendan" + " Eich";
+console.log("Brendan" + " Eich");
 // ```
 //
 // 1.11
 // ```js
-// var firstName = "Brendan";
-// var lastName = "Eich";
-// firstName + " " + lastName;
+var firstName = "Brendan";
+var lastName = "Eich";
+console.log(firstName + " " + lastName);
 // ```
 //
 // 1.12
 // ```js
-// var phoneNumber = "08798976";
-// phoneNumber.length;
+var phoneNumber = "08798976";
+console.log(phoneNumber.length);
 // ```
 //
 // 1.13
 // ```js
-// "Brendan" === "Brendan";
+console.log("Brendan" === "Brendan");
 // ```
 //
 // 1.14 Will the following code log anything out?
 // ```js
-// var fName;
-//
-// if(fName) console.log(`First name is ${fName}`);
+var fName;
+
+if(fName) console.log(`First name is ${fName}`); //no because fname is undefined
 // ```
 //
 // 1.15 Will the following code log anything out?
 // ```js
-// var name = "";
-//
-// if(name) console.log(`First name is ${name}`);
+var name = "";
+
+if(name) console.log(`First name is ${name}`); // no because variable is empty
 // ```
 //
 // ## Exercise 2
@@ -98,6 +98,19 @@
 // If their score is less than 8 they get a "Fail"
 //
 // **Directions:** Given a value for "score"....
+
+var score = 10; //pass on different scores and see what happens:)
+
+if (score == 10) {
+  message = 'Distinction';
+} else if (score == 9) {
+  message = 'Merit';
+} else if (score == 8) {
+  message = 'Pass';
+}else {
+  message = 'Fail';
+}
+console.log('Your score:', message);
 //
 // ```js
 // var score = 10;
@@ -113,12 +126,22 @@
 // Here's an array of objects representing different types of coffee:
 //
 // ```js
-// var coffees = [
-//   { type:"Latte", price:2.65 },
-//   { type:"Cappuccino", price:2.75 },
-//   { type:"Flat white", price:2.95 },
-//   { type:"Super-dooper Mocha Deluxe", price:3.79 }
-// ];
+var coffees = [
+  { type:"Latte", price:2.65 },
+  { type:"Cappuccino", price:2.75 },
+  { type:"Flat white", price:2.95 },
+  { type:"Super-dooper Mocha Deluxe", price:3.79 }
+];
+
+
+// console.log(coffees);
+
+for (var i = 0; i < coffees.length; i++) {
+  var coffeType = coffees[i].type;
+  var coffeePrice = coffees[i].price;
+  console.log(coffeType + 's' + ' cost £' + coffeePrice + ' each');
+}
+
 // ```
 //
 // **Directions:** Loop over the array and print out the following coffee summaries using console.log.
@@ -140,8 +163,63 @@
 // For example if ...
 //
 // ```js
-// var currentPlayer = 'x';
-// var grid = [['x','x','x'], ['o', null, 'o'], [null, null, null]];
+var currentPlayer = 'x';
+
+//constructed grid with every scenario where currentPlayer wins (made easier visually to go through grid...)
+
+var grid = [['x','x','x'],            //Game 1
+            ['o', null, 'o'],
+            [null, null, null],
+//----------------------------------------------
+            [null, null, null],        //Game 2
+            ['o', null, 'o'],
+            ['x', 'x', 'x'],
+//----------------------------------------------
+            ['o', null, 'o'],          //Game 3
+            ['x', 'x', 'x'],
+            [null, null, null],
+//----------------------------------------------
+            ['x', 'o', null],         //Game 4
+            ['x', null, null],
+            ['x', 'o', null],
+//----------------------------------------------
+            [null, 'x','o'],        //Game 5
+            [null, 'x', null],
+            [null, 'x', 'o'],
+//----------------------------------------------
+            [null, 'o','x'],        //Game 6
+            [null, null, 'x'],
+            [null, 'o', 'x'],
+//----------------------------------------------
+            ['x', null,'o'],        //Game 7
+            [null, 'x', null],
+            ['o', null, 'x'],
+//----------------------------------------------
+            ['o', null,'x'],        //Game 8
+            [null, 'x', null],
+            ['x', null, 'o'],
+
+];
+
+
+// now need to work on logic...
+
+if (currentPlayer == grid[0][0]){
+  message = currentPlayer + ' wins';
+}
+console.log(message);
+
+if (currentPlayer == grid[0][1]){
+  message = 'No winner';
+}
+console.log(message);
+
+if (currentPlayer == grid[0][2]){
+  message = 'No game';
+}
+console.log(message);
+
+
 // console.log(grid[0][0]) // prints 'x'
 // ```
 // ...then print "x wins" to the console. If there is no winner, print "No winner".
