8700e62171ba9013fa9d5cdfa9033c43c26a696e
9338190 <a9338190@9338190s-MacBook-Pro.local>
2019-04-03 10:19:35 +0100

    loops.js added-a recap on loops

---
+++ js_fundamentals/loops.js
@@ -0,0 +1,18 @@
+// var languages = ['eng', 'spa', 'ger'];
+// var = events = [
+//   {title: 'The penguin parade', venue: 'the zoo', date: '01/01/1999'}
+//   {title: 'The penguin parade', venue: 'the zoo', date: '01/01/1999'}
+//   {title: 'The penguin parade', venue: 'the zoo', date: '01/01/1999'}
+//   {title: 'The penguin parade', venue: 'the zoo', date: '01/01/1999'}
+// ];
+//
+// for (event of events) {
+//   console.log(`Èvent: ${event.title} at $ {event.venue} on $ );
+// }
+// for (var i = 0; i < languages.length; i++) {
+//   console.log(languages[i]);
+// }
+//
+// for (lang of languages) {
+//   console.log(lang);
+// }
